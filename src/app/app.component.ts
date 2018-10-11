df2c0e655ed68473d0659cc96ac21f12cff2afee
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-10-11 17:50:51 +0200

    any to any

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,34 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   template: `<div align="center"><table bgcolor="green">
               <tr>
                 <td><p>Nazwa komponentu <b>Parrent</b></p><br>
-                  wiadomość: {{message}}
-                  <app-child (messageEvent)="receiveMessage($event)"></app-child>
+                  wiadomość do dziecka: {{message}}
+                  <button type="button" (click)="sendMessage()">wyślij</button>
+                  <app-child></app-child>
                 </td>
               </tr>
             </table></div>
   `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SendDataBetweenComponents';
+export class AppComponent implements OnInit {
 
+  title = 'SendDataBetweenComponents';
   message: string;
 
-  receiveMessage($event) {
-    this.message = $event;
+  ngOnInit() {
+    this.data.currentMessage.subscribe(msg => this.message = msg);
+  }
+
+  constructor(private data: DataService) {
+
+  }
+
+  sendMessage() {
+    this.data.changeMessage('rodzic robi mleko');
   }
 }
