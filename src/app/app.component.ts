730f729e1a03f5cc1145e0e193fbaa9089aec1ab
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-10-11 17:01:37 +0200

    parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div align="center"><table bgcolor="red">
+              <tr>
+                <td><p>Nazwa komponentu <b>Parrent</b></p><br>
+                  <app-child [messageRecive]="message"></app-child>
+                </td>
+              </tr>
+            </table></div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'SendDataBetweenComponents';
+
+   message = 'Hello world';
 }
